642a6e7e17f650d0a5030a44d94f34193fb6a2a5
PtitTux <arnaud.jeanne@gmail.com>
2024-12-14 11:00:27 +0100

    Adjustement types

--- packages/server/app/routes/trackers/find-by-id.ts
+++ packages/server/app/routes/trackers/find-by-id.ts
@@ -1,16 +1,15 @@
 import type { FastifyInstance } from 'fastify'
 import { findByExternalId } from '#Repositories/trackers/find-by-externalId'
 
+interface IParams {
+  id: string
+}
+
 export default async function handler(fastify: FastifyInstance) {
-  fastify.route({
-    method: 'GET',
-    url: '/:id',
-    schema: {
-      tags: ['Trackers'],
-    },
-    handler: async (request, reply) => {
-      const tracker = await findByExternalId(fastify.db, request.params.id)
-      reply.code(200).send(tracker)
-    },
+  fastify.get<
+    { Params: IParams }
+  >('/:id', async (request, reply) => {
+    const tracker = await findByExternalId(fastify.db, request.params.id)
+    reply.code(200).send(tracker)
   })
 }
